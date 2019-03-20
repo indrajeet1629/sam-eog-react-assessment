7333d73420526ab615a0f8857a8613d801c53601
indrajeet1629 <indrajeet_n@yahoo.in>
2019-03-21 01:12:30 +0530

    API Polling

--- src/store/sagas/Drone.js
+++ src/store/sagas/Drone.js
@@ -1,62 +1,49 @@
 import {
-  takeEvery,
+  // takeEvery,
   call,
   put,
   cancel,
-  all
-  //   take,
-  //   race
+  // all,
+  take,
+  race
 } from "redux-saga/effects";
 import API from "../api";
 import * as actions from "../actions";
 
-// function delay(duration) {
-//   const promise = new Promise(resolve => {
-//     setTimeout(() => resolve(true), duration);
-//   });
-//   return promise;
-// }
-
-// function* watchDroneApiRecievedData() {
-//   console.log("it is coming Here watchDroneApiRecievedData");
-//   while (true) {
-//     try {
-//       const { data } = yield call(API.getDrone);
-//       yield put({ type: actions.DRONE_METRIC_DATA, data });
-//       yield call(delay, 5000);
-//     } catch (e) {
-//       yield put({ type: actions.API_ERROR, code: e.code });
-//       yield cancel();
-//       return;
-//     }
-//   }
-// }
-
-function* watchDroneApiRecievedData() {
-  console.log("ControllerCameHere");
-  const { error, data } = yield call(API.getDrone);
-  if (error) {
-    yield put({ type: actions.API_ERROR, code: error.code });
-    yield cancel();
-    return;
-  }
-  yield put({ type: actions.DRONE_METRIC_DATA, data });
+function delay(duration) {
+  const promise = new Promise(resolve => {
+    setTimeout(() => resolve(true), duration);
+  });
+  return promise;
 }
 
-// function* polling() {
-//   //   yield all([takeEvery(actions.FETCH_DRONE_DATA, watchDroneApiRecievedData)]);
-//   while (true) {
-//     console.log("it is coming Here polling");
-//     const data = yield take(actions.START_POLLING);
-//     yield race([
-//       call(watchDroneApiRecievedData, data),
-//       take(actions.STOP_POLLING)
-//     ]);
-//   }
-// }
+function* watchDroneApiRecievedData() {
+  while (true) {
+    try {
+      const { data } = yield call(API.getDrone);
+      yield put({ type: actions.DRONE_METRIC_DATA, data });
+      yield put({
+        type: actions.FETCH_WEATHER,
+        longitude: data.data[0].longitude,
+        latitude: data.data[0].latitude
+      });
+      yield call(delay, 3000);
+    } catch (e) {
+      yield put({ type: actions.API_ERROR, code: e.code });
+      yield cancel();
+      return;
+    }
+  }
+}
+
+function* polling() {
+  while (true) {
+    yield race([call(watchDroneApiRecievedData), take(actions.STOP_POLLING)]);
+  }
+}
+
 function* root() {
-  console.log("it is coming Here root");
-  yield all([takeEvery(actions.FETCH_DRONE_DATA, watchDroneApiRecievedData)]);
+  yield [polling()];
 }
 
 export default [root];
